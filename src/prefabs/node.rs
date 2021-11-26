c35c71aab30ef7881d9fe1ab14f95208f5922b5a
Santiago Calvo <sncalvo@hotmail.com>
2021-11-26 17:46:11 -0300

    Add physics setup

---
+++ src/prefabs/node.rs
@@ -0,0 +1,40 @@
+use bevy::prelude::{shape as bevyShape, *};
+use bevy_rapier3d::prelude::*;
+
+pub fn create_node(
+    commands: &mut Commands,
+    meshes: &mut ResMut<Assets<Mesh>>,
+    materials: &mut ResMut<Assets<StandardMaterial>>,
+) {
+    let rigid_body = RigidBodyBundle {
+        position: Vec3::new(0.0, 10.0, 0.0).into(),
+        ..Default::default()
+    };
+    let collider = ColliderBundle {
+        shape: ColliderShape::ball(0.5),
+        material: ColliderMaterial {
+            restitution: 0.7,
+            ..Default::default()
+        },
+        ..Default::default()
+    };
+
+    commands
+        .spawn_bundle(rigid_body)
+        .insert_bundle(collider)
+        .with_children(|parent| {
+            // child sphere
+            parent.spawn_bundle(PbrBundle {
+                mesh: meshes.add(Mesh::from(bevyShape::Icosphere {
+                    radius: 0.5,
+                    subdivisions: 4,
+                })),
+                material: materials.add(StandardMaterial {
+                    base_color: Color::rgb(0.8, 0.7, 0.6),
+                    ..Default::default()
+                }),
+                ..Default::default()
+            });
+        })
+        .insert(ColliderPositionSync::Discrete);
+}
---
+++ src/prefabs/plane.rs
@@ -0,0 +1,21 @@
+use bevy::prelude::{shape as bevyShape, *};
+use bevy_rapier3d::prelude::*;
+
+pub fn create_plane(
+    commands: &mut Commands,
+    meshes: &mut ResMut<Assets<Mesh>>,
+    materials: &mut ResMut<Assets<StandardMaterial>>,
+) {
+    let collider = ColliderBundle {
+        shape: ColliderShape::cuboid(100.0, 0.1, 100.0),
+        ..Default::default()
+    };
+    commands
+        .spawn_bundle(collider)
+        .insert(ColliderPositionSync::Discrete)
+        .insert_bundle(PbrBundle {
+            mesh: meshes.add(Mesh::from(bevyShape::Plane { size: 100.0 })),
+            material: materials.add(Color::rgb(0.2, 0.2, 0.2).into()),
+            ..Default::default()
+        });
+}
